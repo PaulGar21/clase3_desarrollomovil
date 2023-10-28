5a48a7294ebb83a4fd81b2783bb3362a30f57c6b
ESTUDIANTES <ESTUDIANTES@puce.edu.ec>
2023-10-28 13:32:08 -0500

    initial commit

---
+++ app/src/main/java/com/example/clasesabado28/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.clasesabado28;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int counter=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView txtCounter= findViewById(R.id.txt_view);
+        Button button =findViewById(R.id.btn_counter);
+        Button btnReset = findViewById(R.id.btn_reset);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                txtCounter.setText(counter+"");
+
+            }
+
+
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter=0;
+                txtCounter.setText(counter+"");
+
+            }
+
+
+        });
+    }
+}
